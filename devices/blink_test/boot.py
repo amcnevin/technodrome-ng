7d892453dace70679df94cc28edd61bb97adad27
amcnevin <amcnevin@gmail.com>
2021-05-31 22:52:55 -0400

    adding mqtt tooling

--- devices/blink_test/boot.py
+++ devices/blink_test/boot.py
@@ -1,8 +1,12 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
+import esp
 #esp.osdebug(None)
 import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
+import network
+import ubinascii
+print(ubinascii.hexlify(network.WLAN().config('mac'),":").decode())
+print(esp.flash_id())
 import gc
 #import webrepl
 #webrepl.start()
